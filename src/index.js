bf22bea986184664f2ac1309ee3877e180dac5b1
Anna <anna.argunova@gmail.com>
2019-10-07 19:13:07 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  let varFirst = first.split('').reverse();
+  let varSecond = second.split('').reverse();
+ 
+  let arr = [];
+ 
+  for(let i = 0; i < varFirst.length; i++){
+    for(let k = 0; k < varSecond.length; k++){
+      let temp = varFirst[i] * varSecond[k];
+      arr[i+k] = (arr[i + k]) ? arr[i + k] + temp : temp;
+ 
+    }
+  }
+ 
+  for(let i = 0; i < arr.length; i++){
+    let temp = arr[i] % 10;
+    let moveUp = Math.floor(arr[i] / 10);
+    arr[i] = temp;
+ 
+    if(arr[i +1])
+      arr[i + 1] += moveUp;
+       else if(moveUp != 0) 
+         arr[i + 1] = moveUp;
+       
+ 
+  }
+  let result = arr.reverse().join('');
+  return result;
+ }
+ 
+ 
+
